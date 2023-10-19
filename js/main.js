e255662543d3a97f00b3a3846fc203b1557a0418
abdelazizElsayed <110601463+abdelazizElsayed@users.noreply.github.com>
2023-10-19 16:09:19 +0300

    complete (shop - cart (in navbar - cart page))

--- js/main.js
+++ js/main.js
@@ -36,3 +36,74 @@ function customMaxLength(input) {
   }
 })();
 /* End OTP Function */
+
+/* Start Expanded Nav btn */
+const navExpandedBtn = document.getElementById("nav-expanded-btn");
+let navExpandedIcon = document.getElementById("nav-expanded-btn").children[0];
+const navPages = document.getElementById("nav-pages");
+const allCategoriesHeader = document.getElementById("allCategoriesHeader");
+const expandAllCategoriesHeader = document.getElementById(
+  "expand-allCategories-header"
+);
+
+navExpandedBtn.addEventListener("click", () => {
+  navPages.classList.toggle("opacity-0");
+  allCategoriesHeader.classList.toggle("bg-transparent");
+  expandAllCategoriesHeader.classList.toggle("start-0");
+  navExpandedBtn.getAttribute("aria-expanded") === "true"
+    ? (navExpandedIcon.attributes.src.value = "../imgs/close-menu.svg")
+    : (navExpandedIcon.attributes.src.value = "../imgs/hamburger.svg");
+});
+/* End Expanded Nav btn */
+
+/* Strat Cart */
+const cart = document.getElementById("cart");
+const cartBtn = document.getElementById("cart-btn");
+cartBtn.addEventListener("click", () => {
+  cart.classList.toggle("cart");
+});
+/* End Cart */
+
+/* Strat Filter Price */
+const rangeInput = document.querySelectorAll(".range-input input"),
+  priceInput = document.querySelectorAll(".price-input input"),
+  range = document.querySelector(".slider .progress");
+let priceGap = 50;
+
+priceInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minPrice = parseInt(priceInput[0].value),
+      maxPrice = parseInt(priceInput[1].value);
+
+    if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+      if (e.target.className === "input-min") {
+        rangeInput[0].value = minPrice;
+        range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+      } else {
+        rangeInput[1].value = maxPrice;
+        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+      }
+    }
+  });
+});
+
+rangeInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minVal = parseInt(rangeInput[0].value),
+      maxVal = parseInt(rangeInput[1].value);
+
+    if (maxVal - minVal < priceGap) {
+      if (e.target.className === "range-min") {
+        rangeInput[0].value = maxVal - priceGap;
+      } else {
+        rangeInput[1].value = minVal + priceGap;
+      }
+    } else {
+      priceInput[0].value = minVal;
+      priceInput[1].value = maxVal;
+      range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+      range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+    }
+  });
+});
+/* End Filter Price */
